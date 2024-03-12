b7ebef09c70e790ac689dc3128bf6cee7a165f2c
Jeet Debnath <jeetdeb232@gmail.com>
2024-03-12 17:16:45 +0530

    Updated async queries

--- week-7-recoil/7.4-async-queries/src/atoms.js
+++ week-7-recoil/7.4-async-queries/src/atoms.js
@@ -1,22 +1,26 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
+// Asynchronous data queries
 export const notifications = atom({
     key: "networkAtom",
-    default: {
-        network: 4, 
-        jobs: 6, 
-        messaging: 3, 
-        notifications: 3
-    }
+    default: selector({
+        key: "networkAtomSelector",
+        get: async () => {
+            const res = await axios.get("https://sum-server.100xdevs.com/notifications");
+            return res.data;
+        }
+
+    })
 });
 
 export const totalNotificationSelector = selector({
     key: "totalNotificationSelector",
-    get: ({get}) => {
+    get: ({ get }) => {
         const allNotifications = get(notifications);
-        return allNotifications.network + 
-        allNotifications.jobs + 
-        allNotifications.notifications + 
-        allNotifications.messaging
+        return allNotifications.network +
+            allNotifications.jobs +
+            allNotifications.notifications +
+            allNotifications.messaging
     }
 })
