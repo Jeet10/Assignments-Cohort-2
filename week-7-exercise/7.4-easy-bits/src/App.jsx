48977770ff6728c818cd3a8a8130407484bd727f
Jeet Debnath <jeetdeb232@gmail.com>
2024-03-12 15:38:29 +0530

    Updated

--- week-7-exercise/7.4-easy-bits/src/App.jsx
+++ week-7-exercise/7.4-easy-bits/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  return (
+    <RecoilRoot>
+      <NavBar />
+    </RecoilRoot>
+  )
+}
+
+function NavBar() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
+  const notificationsCount = useRecoilValue(notificationsAtom)
+  const [messagingCount, setMessagingCount] = useRecoilState(messagingAtom)
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button>Home</button>
+      <button>My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs ({jobsCount >= 100 ? "99+" : jobsCount})</button>
+      <button>Messaging ({messagingCount >= 100 ? "99+" : messagingCount})</button>
+      <button>Notifications ({notificationsCount >= 100 ? "99+" : notificationsCount})</button>
+      <button onClick={() => setMessagingCount(messagingCount + 1)}>Me ({totalNotificationCount})</button>
     </>
   )
 }
